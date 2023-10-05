fe50e40b102fcef352eff786bcd304227a25d815
kaytee07 <kofi.taylor07@gmail.com>
2023-10-05 11:21:35 +0000

    function notations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+type annotated function that takes in a function add that takes a
+float a and b as argument and returns their sum as a float
+"""
+
+
+def add(a:float, b:float) -> float:
+    return a + b
