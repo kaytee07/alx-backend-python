c43479abdca8fd82923a0e708b22a0a28ba1ed2f
kaytee07 <kofi.taylor07@gmail.com>
2023-10-05 14:02:51 +0000

    type checking

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-type annotated function that takes str1 and a str2 as arguments 
+type annotated function that takes str1 and a str2 as arguments
 and returns a concatenated string
 """
 
