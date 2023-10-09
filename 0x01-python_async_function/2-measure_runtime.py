dc3f96383a6a5b487ca1d058d3ffff734de53d6b
kaytee07 <kofi.taylor07@gmail.com>
2023-10-09 17:05:18 +0000

    trailing space error

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-measure_time function with integers n and max_delay as 
-arguments that measures the total execution time for 
+measure_time function with integers n and max_delay as
+arguments that measures the total execution time for
 wait_n(n, max_delay), and returns total_time / n.
 """
 import time
