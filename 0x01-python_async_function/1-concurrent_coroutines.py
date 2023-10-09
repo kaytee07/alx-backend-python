6e4dad866457c984e401c35b57228b4af2b90c21
kaytee07 <kofi.taylor07@gmail.com>
2023-10-09 16:55:38 +0000

    concurrent push

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,12 +4,12 @@ async routine called wait_n that takes in 2 int arguments (in this
 order): n and max_delay. You will spawn wait_random n times with
 the specified max_delay
 """
-
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     return wait_random n times
 
