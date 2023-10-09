924b2ad0ac53d74dd2c5adbf5e3c651e4e068aaf
kaytee07 <kofi.taylor07@gmail.com>
2023-10-09 15:55:13 +0000

    concurrent routines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+async routine called wait_n that takes in 2 int arguments (in this
+order): n and max_delay. You will spawn wait_random n times with
+the specified max_delay
+"""
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    """
+    return wait_random n times
+
+    Args:
+        n (int): number of times to return wait_random
+
+    Return:
+         wait_random (list): list of wait random n times
+    """
+    list_of_rand = []
+    for i in range(n):
+        list_of_rand.append(await wait_random(max_delay))
+    return list_of_rand
