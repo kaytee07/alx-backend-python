fb9bd9530d695d43f1486c2c340f0d8de2ccfc5e
kaytee07 <kofi.taylor07@gmail.com>
2023-10-09 17:28:57 +0000

    3-task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,7 +7,7 @@ integer max_delay and returns a asyncio.Task.
 import asyncio
 
 
-wait_random = __import__('0-basic_async_suntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
@@ -20,6 +20,7 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     Returns:
         asyncio.Task: Task object for the wait_random coroutine.
     """
+    return asyncio.create_task(wait_random(max_delay))
     if max_delay < 0:
         raise ValueError("max_delay cannot be negative")
 
