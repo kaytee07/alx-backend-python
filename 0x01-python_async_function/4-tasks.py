88517f0b761565868a2cc9e57d35e6591ae0dffb
kaytee07 <kofi.taylor07@gmail.com>
2023-10-09 17:33:18 +0000

    bug fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,7 +6,7 @@ the specified max_delay
 """
 from typing import List
 
-task_wait_random = __import__('3-task').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
