5078fdc870b43eb4e10992cb4c1bbe27d6c34c48
kaytee07 <kofi.taylor07@gmail.com>
2023-10-10 10:15:56 +0000

    get values using async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+async_generator that takes no arguments.
+"""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """
+    function loop 10 times each time wait one second before 
+    returning a random number between 1 and 10
+
+    Args:
+        no args
+
+    Return:
+        return random floating point numbers betwen 1 - 10
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
