2e1927d8124d868ac430533e5510a97f4fcba860
kaytee07 <kofi.taylor07@gmail.com>
2023-10-30 15:16:09 +0000

    test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,83 +1,84 @@
 #!/usr/bin/env python3
+"""A module for testing the utils module.
 """
-Test file for github org client
-"""
-from parameterized import parameterized
 import unittest
-from client import GithubOrgClient
-from unittest.mock import patch, PropertyMock
+from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
 
 
-class TestGithubOrgClient(unittest.TestCase):
-    """
-    Implementation of github org client class
-    """
-
+class TestAccessNestedMap(unittest.TestCase):
+    """Tests the `access_nested_map` function."""
     @parameterized.expand([
-        ("google", "google"),
-        ("abc", "abc")
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    @patch('client.get_json')
-    def test_org(self, _: str, org_name: str, mock_get_json):
-        """
-        Tests that GithubOrgClient.org returns correct value
-        """
-        mock_url = "https://api.github.com/orgs/{}/repos".format(org_name)
-        mock_get_json.return_value = {"repos_url": mock_url}
-        gitclient = GithubOrgClient(org_name)
-        org_repo = gitclient.org
-        self.assertEqual(org_repo, {"repos_url": mock_url})
-        mock_get_json.assert_called_with
-        (gitclient.ORG_URL.format(org=org_name))
-
-    def test_public_repos_url(self):
-        """
-        Method to unit test GithubOrgClient._public_repos_url
-        """
-        mock_url = "https://api.github.com/orgs/google/repos"
-        test_payload = {"repos_url": mock_url}
-        with patch('test_client.GithubOrgClient.org',
-                   new_callable=PropertyMock) as mock_org:
-            mock_org.return_value = test_payload
-            git_client = GithubOrgClient("google")
-            url = git_client._public_repos_url
-            self.assertEqual(url, test_payload["repos_url"])
-
-    @patch('client.get_json')
-    def test_public_repos(self, mock_get_json):
-        """
-        Method to unit test GithubOrgClient.public_repos
-        """
-        mock_url = "https://api.github.com/orgs/google/repos"
-        payload = {mock_url: [{"name": "google/episodes.dart", "license": {
-          "key": "bsd-3-clause",
-          "name": "BSD 3-Clause \"New\" or \"Revised\" License",
-          "spdx_id": "BSD-3-Clause",
-          "url": "https://api.github.com/licenses/bsd-3-clause",
-          "node_id": "MDc6TGljZW5zZTU="
-        }}, {"name": "cpp-netlib", "license": None},
-            {"name": "ios-webkit-debug-proxy"}]}
-        mock_get_json.return_value = payload[mock_url]
-        with patch('test_client.GithubOrgClient._public_repos_url',
-                   new_callable=PropertyMock) as mock_public_repo:
-            mock_public_repo.return_value = mock_url
-            gitcli = GithubOrgClient("google")
-            names_list = gitcli.public_repos()
-            expected_list = ["google/episodes.dart",
-                             "cpp-netlib", "ios-webkit-debug-proxy"]
-            self.assertEqual(names_list, expected_list)
-            mock_public_repo.assert_called_once()
-        mock_get_json.assert_called_once()
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """Tests `access_nested_map`'s output."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-            ("lic_true", {"license": {"key": "my_license"}},
-                "my_license", True),
-            ("lic_false", {"license": {"key": "other_license"}},
-                "my_license", False)
-        ])
-    def test_has_license(self, _, repo, license_key, expected):
-        """
-        Method to unit test GithubOrgClient.has_license
-        """
-        gitcli = GithubOrgClient("google")
-        self.assertEqual(gitcli.has_license(repo, license_key), expected)
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests the `get_json` function."""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
+        """Tests `get_json`'s output."""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
